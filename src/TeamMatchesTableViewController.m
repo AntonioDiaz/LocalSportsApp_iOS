dadeab2e4e472600e6cb3563d633c0938eca87ab
Antonio Díaz Arroyo <antonio.diaz.arroyo@gmal.com>
2018-01-19 08:41:49 +0100

    fix bug: show alert on favorit team screen

--- src/TeamMatchesTableViewController.m
+++ src/TeamMatchesTableViewController.m
@@ -27,12 +27,9 @@
 #pragma mark - private methods
 -(void) updateFavoriteImage {
     NSString *favoriteImage = @"favorite_unselect";
-    NSString *msg = NSLocalizedString(@"FAVORITES_TEAM_REMOVED", nil);
     if (isFavoriteTeam) {
         favoriteImage = @"favorite";
-        msg = NSLocalizedString(@"FAVORITES_TEAM_ADDED", nil);
     }
-    [Utils showAlert:msg];
     UIImage *image = [[UIImage imageNamed:favoriteImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     image = [Utils imageWithSize:image scaledToSize:CGSizeMake(30, 30)];
     buttonFavorite = [[UIBarButtonItem alloc]initWithImage:image
@@ -49,6 +46,12 @@
     [UtilsDataBase markOrUnmarkTeamAsFavorite:teamName
                               withCompetition:self.competitionEntity.idCompetitionServer
                                    isFavorite:isFavoriteTeam];
+    
+    NSString *msg = NSLocalizedString(@"FAVORITES_TEAM_REMOVED", nil);
+    if (isFavoriteTeam) {
+        msg = NSLocalizedString(@"FAVORITES_TEAM_ADDED", nil);
+    }
+    [Utils showAlert:msg];
     [self updateFavoriteImage];
 }
 
